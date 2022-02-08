e91a1c393fa95c529fbfe4f3d25ae696950dc478
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-02-08 19:28:59 +0530

    re-indent to tabs
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

--- src/test/java/com/thehecklers/sburrestdemo/SburRestDemoApplicationTests.java
+++ src/test/java/com/thehecklers/sburrestdemo/SburRestDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SburRestDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
